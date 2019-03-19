b0db09a703d9f43d6e9a3d990610823dc411cfea
Giu1983 <42543829+Giu1983@users.noreply.github.com>
2019-03-19 15:52:45 +0100

    Update hello.js

--- hello.js
+++ hello.js
@@ -12,7 +12,7 @@ clientData = axios.create ({
   headers: {
     Accept: "application/json", 
      app_id: "b91ba6fb", 
-        app_key:"18cba4081827e5572ab9733dae095075"
+     app_key:"18cba4081827e5572ab9733dae095075"
     }
 
 }); 
@@ -20,8 +20,8 @@ clientData = axios.create ({
 clientData.get("hello").then(res=> {
     var data1 = res.data.results[0].lexicalEntries; 
     data1.forEach(element => {
-        console.log(element.lexicalWord); 
-        console.log(element.entries[0].senses[0].short_definitions[0]); 
+    console.log(element.lexicalWord); 
+    console.log(element.entries[0].senses[0].short_definitions[0]); 
     });
     console.log("Provided by " + res.data.metadata.provider); 
 });  
